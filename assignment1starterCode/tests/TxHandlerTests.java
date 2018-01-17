c8a6494ec8e3d686ae1f40fe7979d3245b8245b5
Alex <alex@localhost.com>
2018-01-17 23:03:37 +0100

    added test for Crypto class

--- assignment1starterCode/tests/TxHandlerTests.java
+++ assignment1starterCode/tests/TxHandlerTests.java
@@ -1,9 +1,26 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.security.*;
+
 public class TxHandlerTests {
     @Test
-    public void testMe() {
-        Assert.assertTrue(true);
+    public void testCrypto() throws Exception {
+        final String MESSAGE = "Hello World!";
+
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048, new SecureRandom());
+        KeyPair pair = generator.generateKeyPair();
+
+        PublicKey publicKey = pair.getPublic();
+
+        Signature privateSignature = Signature.getInstance("SHA256withRSA");
+        privateSignature.initSign(pair.getPrivate());
+        privateSignature.update(MESSAGE.getBytes());
+
+        byte[] signature = privateSignature.sign();
+
+        Assert.assertTrue(Crypto.verifySignature(publicKey, MESSAGE.getBytes(), signature));
+        Assert.assertFalse(Crypto.verifySignature(publicKey, "FALSE".getBytes(), signature));
     }
 }
